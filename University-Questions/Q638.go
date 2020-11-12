2a4625aba9773e852d22ee7f66ed3b7b1149875f
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2020-11-12 09:40:49 +0330

    Create Q638.go

---
+++ University-Questions/Q638.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"strconv"
+)
+
+// I used golang because of big.Int struct
+func main() {
+	triangleRow := make([]*big.Int, 100 + 1)
+	sum := new(big.Int).SetInt64(1)
+	triangleRow[0] = new(big.Int).SetInt64(1)
+	triangleRow[1] = new(big.Int).SetInt64(1)
+	var n int
+	{
+		var input string
+		fmt.Scanln(&input)
+		n, _ = strconv.Atoi(input)
+	}
+	if n == 0{
+		fmt.Println("0")
+		return
+	}
+  // use pascal's triangle to calculate the series
+	for i := 2; i <= n; i++ {
+		temp := new(big.Int).SetInt64(1)
+		for  j := 1; j < i; j++{
+			veryTemp := new(big.Int).SetBytes(triangleRow[j].Bytes()) // copy the big int
+			triangleRow[j].Add(triangleRow[j], temp)
+			temp = veryTemp
+		}
+
+		triangleRow[i] = new(big.Int).SetInt64(1)
+		multiply := new(big.Int).SetInt64(1)
+    // this could be ompimized a bit by going halfway and multiplying raising the answer to power of 2. (and multiply the middle element if necessary)
+		for j := 0; j <= i; j++ {
+			multiply.Mul(multiply, triangleRow[j])
+		}
+		sum.Add(sum,multiply)
+	}
+	fmt.Println(sum.String())
+}
