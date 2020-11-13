e46e5a361982275434493cf4d7e1c54ea4623c44
HirbodBehnam <chrome.hiri.angry@gmail.com>
2020-11-13 18:02:26 +0330

    Added problem 66867

---
+++ University-Questions/Q66867.h
@@ -0,0 +1,32 @@
+#ifndef QUERA_Q66867_H
+#define QUERA_Q66867_H
+
+#endif //QUERA_Q66867_H
+
+#define MAX_FLOWERS 20
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int flowers_count, months;
+    scanf("%d %d", &flowers_count, &months);
+    int white_occurrences[MAX_FLOWERS]; // count the occurrences for the white flowers. The index is the flower index
+    memset(white_occurrences, 0, sizeof(white_occurrences));
+    // read months
+    for(; months > 0; months--){
+        char status[MAX_FLOWERS];
+        scanf("%s", status);
+        for(int i = 0; i < flowers_count; i++)
+            if(status[i] == 'W')
+                white_occurrences[i]++;
+    }
+    // check white occurrences
+    for(int i = 0; i < flowers_count; i++)
+        if(white_occurrences[i] % 2 == 0) // white_occurrences[i] & 1 == 0?
+            putchar('B');
+        else
+            putchar('F');
+
+    return 0;
+}
