7817b3cdfd1dc4f12f4d9f438fe1c5c46acb827e
HirbodBehnam <chrome.hiri.angry@gmail.com>
2020-11-13 17:40:40 +0330

    Added problem 66866

---
+++ University-Questions/Q66866.h
@@ -0,0 +1,30 @@
+#ifndef QUERA_Q66866_H
+#define QUERA_Q66866_H
+
+#endif //QUERA_Q66866_H
+
+#define MAX_A 100000
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    unsigned long long edges_count = 0;
+    // create an array to store the occurrences of the numbers. The length of the array must be the max A + 1
+    int* occurrences = malloc((MAX_A + 1) * sizeof(int)); // no need to free this. We need it until the very end
+    memset(occurrences, 0, (MAX_A + 1) * sizeof(int));
+    // read the input and calculate the occurrences
+    for(; n > 0; n--){
+        int a;
+        scanf("%d", &a);
+        occurrences[a]++;
+    }
+    // now see how many of them have the difference of 1
+    // note that i use MAX_A not MAX_A + 1. Because I don't want to check the last element
+    for(int i = 1; i < MAX_A; i++)
+        edges_count += occurrences[i] * occurrences[i + 1];
+    printf("%llu", edges_count);
+}
