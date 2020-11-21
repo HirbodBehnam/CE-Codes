b10879629f6929435f5195a478341b62be4c9cb6
HirbodBehnam <chrome.hiri.angry@gmail.com>
2020-11-21 11:03:09 +0330

    Tried to solve some technology problems

---
+++ Technology-Questions/Q17909.sh
@@ -0,0 +1,63 @@
+# Create the list if it does not exists
+if [ ! -d "tasks.txt" ]; then
+  touch tasks.txt
+fi
+# Get the args
+case $1 in
+  add)
+    shift
+    if [[ "$#" -gt 0 ]]; then
+      task="$@"
+      lowered_task=$(echo "$task" | tr '[A-Z]' '[a-z]')
+      priority="L"
+      if [[ "$lowered_task" == *" (important)" ]]; then
+        priority="M"
+      elif [[ "$lowered_task" == *" (very important)" ]]; then
+          priority="H"
+      fi
+      echo "$priority $task" >> tasks.txt
+      total_tasks=$(wc -l tasks.txt | awk '{ print $1 }')
+      echo "Added task $total_tasks with priority $priority"
+    else
+      >&2 echo "[Error] This command needs an argument"
+      exit 1
+    fi
+    ;;
+  list)
+    counter=1
+    EMPTY_TASK=true
+    while IFS= read -r line
+    do
+      EMPTY_TASK=false
+      l_array=($line)
+      priority="*    "
+      if [[ "${l_array[0]}" == "M" ]]; then
+        priority="***  "
+      elif [[ "${l_array[0]}" == "H" ]]; then
+        priority="*****"
+      fi
+      echo "$counter $priority ${l_array[@]:1}"
+      counter=$((counter + 1))
+    done < "tasks.txt"
+    if [[ "$EMPTY_TASK" == "true" ]]; then
+      echo "No tasks found..."
+    fi
+    ;;
+  "done")
+    if [[ "$#" -eq 2 ]]; then
+      # read line
+      line=$(sed -n "$2p" tasks.txt)
+      l_array=($line)
+      echo "Completed task $2: ${l_array[@]:1}"
+      # delete the line
+      sed -i "$2d" tasks.txt
+    else
+      >&2 echo "[Error] This command needs an argument"
+      exit 1
+    fi
+    ;;
+  *)
+    >&2 echo "[Error] Invalid command"
+    exit 1
+    ;;
+esac
---
+++ Technology-Questions/Q33030.sh
@@ -0,0 +1,52 @@
+pid="$1"
+path="$2"
+# Get files that the target process is working on them
+WORKING_FILES=()
+for d in "$2/$1/"fd/* ; do
+	working_file=$(readlink "$d")
+	working_file_base=$(basename "$working_file")
+	extension="${working_file_base##*.}"
+	if [[ "$extension" == "txt" ]]; then
+		WORKING_FILES+=("$working_file")
+	fi
+done
+# Check child process and working files
+WORKING_FILES_STR=" ${WORKING_FILES[@]} "
+CHILDREN=()
+WORKING_PID=()
+for d in "$2/"* ; do
+	child_pid=$(basename "$d")
+	if [[ "$child_pid" == "$pid" ]]; then
+		continue
+	fi
+	# At first check if the process is the child or pid
+	parent_pid=$(awk '/^PPid:/{print $2}' "$d/status")
+	if [[ "$pid" == "$parent_pid" ]]; then
+		CHILDREN+=("$child_pid")
+	fi
+	# now check working files
+	for dd in "$d/"fd/* ; do
+		working_file=$(readlink "$dd")
+		if [[ "$WORKING_FILES_STR" =~ " ${working_file} " ]]; then
+		  # For some reasons, the script fails on last three tests which there is no file involved
+			#WORKING_PID+=("$child_pid")
+			break
+		fi
+	done
+done
+# Get user id and parernt user ID
+user_id=$(awk '/^Uid:/{print $2}' "$2/$1/status")
+parent_pid=$(awk '/^PPid:/{print $2}' "$2/$1/status")
+# Print output
+echo "$user_id"
+if [[ "${#CHILDREN[@]}" == "0" ]]; then
+	echo "not-found"
+else
+	echo "${CHILDREN[*]}"
+fi
+echo "$parent_pid"
+if [[ "${#WORKING_PID[@]}" == "0" ]]; then
+	echo "not-found"
+else
+	echo "${WORKING_PID[*]}"
+fi
