8e6102699b4638e30fa6d8cf94236a9e84bb061d
HirbodBehnam <chrome.hiri.angry@gmail.com>
2020-11-22 12:11:05 +0330

    Added a shitty question and answer

---
+++ University-Questions/Q652.h
@@ -0,0 +1,34 @@
+#ifndef QUERA_Q652_H
+#define QUERA_Q652_H
+
+#endif //QUERA_Q652_H
+
+#include <math.h>
+#include <stdio.h>
+
+// I used bruteforce here; I don't know if there is a better way or not
+int main() {
+    int input[100];
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &input[i]);
+    unsigned long long EndInterval = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            EndInterval += input[i] * input[j];
+    EndInterval *= 2;
+
+    for (unsigned long long i = 100; i <= EndInterval; i++) {
+        int digits = log10(i);
+        digits++;
+        unsigned long long temp = i, sum = 0;
+        while (temp != 0) {
+            sum += pow(temp % 10, digits);
+            temp /= 10;
+        }
+        if (sum == i)
+            printf("%llu\n", i);
+    }
+    return 0;
+}
