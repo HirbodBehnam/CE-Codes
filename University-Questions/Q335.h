f4a268c452ba439b6dafd763fa310b12b7ed07c8
HirbodBehnam <chrome.hiri.angry@gmail.com>
2020-11-30 14:56:46 +0330

    Fixed 335

--- University-Questions/Q335.h
+++ University-Questions/Q335.h
@@ -7,33 +7,33 @@
 
 #include <stdio.h>
 
-int run() {
+int main() {
     /**
      * So this array somehow plays the role of stack type in higher level languages
      * The strategy in this question is to use a stack like object to keep track of sums of sub-sets
      */
     unsigned long long sums[MAX], temp_number;
-    int index = -1;
+    int stack_size = 0;
     // read input char to char
     int c;
     while ((c = getchar()) != EOF) {
         if (c == '{') {
             // same as pushing zero into stack
-            index++;
-            sums[index] = 0;
+            stack_size++;
+            sums[stack_size - 1] = 0;
             temp_number = 0;
         } else if (c == '}') {
-            sums[index] += temp_number;
+            sums[stack_size - 1] += temp_number;
+            printf("%llu\n", sums[stack_size - 1]);
             // same as popping last element from stack
-            printf("%llu\n", sums[index]);
-            sums[index - 1] += sums[index]; // add the sum to element before (to the set before)
-            index--;
+            stack_size--;
+            if(stack_size != 0)
+                sums[stack_size - 1] += sums[stack_size]; // add the sum to element before (to the set before)
             temp_number = 0;
         } else if (c == ',') { // done reading the number, add it into stack
-            sums[index] += temp_number;
+            sums[stack_size - 1] += temp_number;
             temp_number = 0;
-            getchar(); // this skips the space after ,
-        } else { // read the number
+        } else if(c >= '0' && c <= '9') { // read the number
             temp_number *= 10;
             temp_number += c - '0';
         }
