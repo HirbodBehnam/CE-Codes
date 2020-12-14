d0936b287615e31c1cb3476b624c7b6fb850acc5
HirbodBehnam <chrome.hiri.angry@gmail.com>
2020-12-14 21:26:40 +0330

    Better code

--- Assignments/CE/Series3-1/Q5.h
+++ Assignments/CE/Series3-1/Q5.h
@@ -3,6 +3,7 @@
 
 #endif //QUERA_Q5_H
 
+#include <stdbool.h>
 #include <stdio.h>
 
 int main() {
@@ -10,20 +11,20 @@ int main() {
     scanf("%d", &n);
     getchar(); // skip the \n after the number
     for (; n > 0; n--) {
-        char fatemeh_have_odd = 0, mohamad_have_even = 0, digits_total = 0;
+        bool fatemeh_have_odd = false, mohamad_have_even = false, fatemeh_round = true;
         // count the odd and even digits
-        char c, fatemeh_round = 1;
+        char c, digits_total = 0;
         // this counts the input from left to right
         while ((c = getchar()) != '\n' && c != EOF) {
             int digit = c - '0';
             if (fatemeh_round) {
                 if (digit % 2 == 1)
-                    fatemeh_have_odd = 1; // fatemeh can win the round if number of digits is odd
-                fatemeh_round = 0;
+                    fatemeh_have_odd = true; // fatemeh can win the round if number of digits is odd
+                fatemeh_round = false;
             } else {
                 if (digit % 2 == 0)
-                    mohamad_have_even = 1; // mohamad can win the round if number of digits is even
-                fatemeh_round = 1;
+                    mohamad_have_even = true; // mohamad can win the round if number of digits is even
+                fatemeh_round = true;
             }
             digits_total++;
         }
@@ -34,9 +35,9 @@ int main() {
          * So if fatemeh have at least one odd number it it's numbers (indexes) can win the game by leaving that number as the last number
          */
         if (digits_total % 2 == 1)
-            printf(fatemeh_have_odd > 0 ? "1\n" : "2\n");
+            printf(fatemeh_have_odd ? "1\n" : "2\n");
         else
-            printf(mohamad_have_even > 0 ? "2\n" : "1\n");
+            printf(mohamad_have_even ? "2\n" : "1\n");
         /**
          * If the count of digits is even, it means that one of the digits from even indexes (mohamad numbers) will be remained at last
          * This means that fatemeh have no control over the game, because in all scenarios, fatemeh have to eliminate all of his numbers
