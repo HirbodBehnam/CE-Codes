80e1c03638cfcb520cf52df549b8eda8efd3cf6c
HirbodBehnam <chrome.hiri.angry@gmail.com>
2021-01-03 16:01:33 +0330

    Better code

--- University-Questions/Q9722.h
+++ University-Questions/Q9722.h
@@ -3,140 +3,65 @@
 
 #endif //QUERA_Q9722_H
 
-#define PRIME 0
-#define NOT_PRIME 1
-
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- * Checks if a number is right truncatable
- * @param candidates Sieve result
- * @param candidate The number to check
+ * Returns 0 if the number is prime. Returns it's first factor if not
+ * @param number The number to check
+ * @return 0 if the number is prime otherwise it's first factor
  */
-void check(const char *candidates, int candidate) {
-    int backup = candidate;
-    while (candidate != 0) {
-        if (candidate == 1)
-            return;
-        if (candidate == 2 || candidate == 3)
-            break;
-        if (candidate % 2 == 0 || candidate % 3 == 0)
-            return;
-        if (candidates[candidate] == NOT_PRIME)
-            return;
-        candidate /= 10;
+unsigned long long is_prime(unsigned long long x) {
+    if (x == 1)
+        return 1;
+    if (x == 2 || x == 3 || x == 5 || x == 7)
+        return 0;
+    if (x % 2 == 0)
+        return 2;
+    if (x % 3 == 0)
+        return 3;
+    const unsigned long long To = (unsigned long long) sqrt((double) x);
+    for (unsigned long long i = 5; i <= To; i += 4) {
+        if (x % i == 0)
+            return i;
+        i += 2;
+        if (x % i == 0)
+            return i;
     }
-    printf("%d\n", backup);
+    return 0;
+}
+
+/**
+ * Backtrack right truncatable numbers
+ * @param number The number to check
+ * @param n Number of digits left
+ */
+void backtrack_numbers(unsigned long long number, int n) {
+    // end the recursion
+    if (n == 0) {
+        printf("%llu\n", number);
+        return;
+    }
+    n--;
+    number *= 10;
+    if (is_prime(number + 1) == 0)
+        backtrack_numbers(number + 1, n);
+    if (is_prime(number + 3) == 0)
+        backtrack_numbers(number + 3, n);
+    if (is_prime(number + 7) == 0)
+        backtrack_numbers(number + 7, n);
+    if (is_prime(number + 9) == 0)
+        backtrack_numbers(number + 9, n);
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    char *candidates;
-    int from = (int) pow(10.0, n - 1);
-    // do sieve
-    {
-        candidates = calloc(sizeof(char), from * 10);
-        int length = from * 10 + 1;
-        const int To = (int) sqrt((double) length);
-        int i;
-        for (i = 5;; i += 4) // only check 6k +- 1
-        {
-            int j;
-            if (candidates[i] == 0) {
-                // check if eliminating candidates is needed
-                if (i <= To) {
-                    // eliminate other non-primes
-                    j = 2;
-                    do {
-                        candidates[j * i] = NOT_PRIME;
-                        j++;
-                    } while (j * i < length);
-                } else
-                    break;
-
-            }
-            i += 2;
-            if (candidates[i] == 0) {
-                // check if eliminating candidates is needed
-                if (i <= To) {
-                    // eliminate other non-primes
-                    j = 2;
-                    do {
-                        candidates[j * i] = NOT_PRIME;
-                        j++;
-                    } while (j * i < length);
-                } else
-                    break;
-            }
-        }
-    }
-    // check numbers
-    // numbers which end in 2
-    {
-        int candidate = from * 2;
-        while (candidate % 6 != 5) {
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-            candidate++;
-        }
-        for (; candidate < from * 3; candidate += 4) {
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-            candidate += 2;
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-        }
-    }
-    // numbers which end in 3
-    {
-        int candidate = from * 3;
-        while (candidate % 6 != 5) {
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-            candidate++;
-        }
-        for (; candidate < from * 4; candidate += 4) {
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-            candidate += 2;
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-        }
-    }
-    // numbers which end in 5
-    {
-        int candidate = from * 5;
-        while (candidate % 6 != 5) {
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-            candidate++;
-        }
-        for (; candidate < from * 6; candidate += 4) {
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-            candidate += 2;
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-        }
-    }
-    // numbers which end in 7
-    {
-        int candidate = from * 7;
-        while (candidate % 6 != 5) {
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-            candidate++;
-        }
-        for (; candidate < from * 8; candidate += 4) {
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-            candidate += 2;
-            if (candidates[candidate] == PRIME)
-                check(candidates, candidate);
-        }
-    }
+    n--;
+    backtrack_numbers(2, n);
+    backtrack_numbers(3, n);
+    backtrack_numbers(5, n);
+    backtrack_numbers(7, n);
     return 0;
 }
